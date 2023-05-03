9ff7448dfc0cee0016f44e759af6928f5ec8c5c2
Max Minh Tran <111483518+ItzTJSV2@users.noreply.github.com>
2023-05-03 17:22:35 +0700

    Update CIFAR100-CNN.py

--- CIFAR100-CNN.py
+++ CIFAR100-CNN.py
@@ -16,22 +16,32 @@ from torchsummary import summary
 from datetime import datetime
 import csv
 
+base_dir = os.getcwd()
 device = torch.device('cuda')
 
-
+frozen = False
 
 # These are the variables that will be changed
-learning_rate = 1e-4 #1e-3
+# In order to stop the training early, do CTRL+C
+learning_rate = 1e-5 #1e-5
 batch_size = 10 # 10
-num_epochs = 50 # (x % 4 = 0)
-Trained_Model = 1  # ResNet50, VGG16, InceptionV3 (1-3)
-Train_type = 1 # Train whole Model, Train w/ Freeze, Train w/ Freeze then Unfreeze  (1-3)
+num_epochs = 100 # 100 / 300
+Trained_Model = 2  # ResNet50, VGG16, InceptionV3 (1-3)
+Train_type = 2 # Train whole Model with no Weights, Train w/ Freeze, Train w/ Freeze then Unfreeze  (1-3)
 
+ContinueTrain = False
+
+total_train_hr = 0
+total_train_min = 0
+total_train_sec = 0
 
 # Defining the model, their corresponding transformations, and adding layers to transform the model to 100 classes
 # Each transformation parameter was taken from their respective documentation page https://pytorch.org/vision/main/models.html 
 if (Trained_Model == 1):
-    model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+    if Train_type != 1:
+        model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+    else:
+        model = models.resnet50(weights=None)
     model.to(device)
     # Transformations applied to the training set are often randomized to reduce over-fitting
     transform = transforms.Compose([
@@ -59,12 +69,15 @@ if (Trained_Model == 1):
         nn.Dropout(p=0.4),
         nn.Linear(256, 100))
     
-    Direc = r'C:\Users\maxtr\Pytorch Projects\EE Neural Networks\ResNet50'
+    Direc = os.path.join(base_dir, 'ResNet50')
 elif (Trained_Model == 2):
-    model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
+    if Train_type != 1:
+        model = models.vgg16(weights=models.VGG16_Weights.DEFAULT)
+    else:
+        model = models.vgg16(weights=None)
     model.to(device)
     transform = transforms.Compose([
-        transforms.Resize(232),
+        transforms.Resize(256),
         transforms.RandomCrop(224, pad_if_needed=True),
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.ColorJitter(brightness=(0.5, 1.5), contrast=1, saturation=(0.5, 1.5), hue=0.1),
@@ -83,9 +96,12 @@ elif (Trained_Model == 2):
         nn.Linear(256, 100)
     )
     
-    Direc = r'C:\Users/maxtr\Pytorch Projects\EE Neural Networks\VGG16'
+    Direc = os.path.join(base_dir, 'VGG16')
 elif (Trained_Model == 3):
-    model = models.inception_v3(weights=models.Inception_V3_Weights.DEFAULT)
+    if Train_type != 1:
+        model = models.inception_v3(weights=models.Inception_V3_Weights.DEFAULT)
+    else:
+        model = models.inception_v3(weights=None)
     model.to(device)
     transform = transforms.Compose([
         transforms.Resize(342),
@@ -109,23 +125,27 @@ elif (Trained_Model == 3):
         nn.Dropout(p=0.4),
         nn.Linear(256, 100))
         
-    Direc = r'C:\Users/maxtr\Pytorch Projects\EE Neural Networks\InceptionV3'
-
+    Direc = os.path.join(base_dir, 'InceptionV3')
+    
+    model.aux_logits = False
+    
 # Moving the network model's processing to the GPU and setting it to train mode
 model.to(device)
 model.train()
     
 # Downloading and loading the CIFAR100 dataset from : https://www.cs.toronto.edu/~kriz/cifar.html (python version)
-DLTraining = datasets.CIFAR100(root=r'C:\Users\maxtr\Desktop\Datasets\CIFAR100', train=True, download=True, transform=transform)
-DLTest = datasets.CIFAR100(root=r'C:\Users\maxtr\Desktop\Datasets\CIFAR100', train=False, download=True, transform=transform_test)
+DLTraining = datasets.CIFAR100(root=os.path.join(base_dir, 'CIFAR100'), train=True, download=True, transform=transform)
+DLTest = datasets.CIFAR100(root=os.path.join(base_dir, 'CIFAR100'), train=False, download=True, transform=transform_test)
 
 TrainingData = DataLoader(dataset=DLTraining, batch_size=batch_size, shuffle=True)
-TestData = DataLoader(dataset=DLTest, batch_size=batch_size)
+TestData = DataLoader(dataset=DLTest, batch_size=batch_size, shuffle=True)
 
+avgtestloss = []
 def get_accuracy(loader, model):
     correct_count = 0
     total = 0
     model.eval() # Set the model to evaluation mode
+    testlosses = []
     
     bar = tqdm(loader, total=len(loader), leave=False)
     
@@ -135,32 +155,41 @@ def get_accuracy(loader, model):
             y = y.to(device=device)
 
             scores = model(x)
-            _, predictions = scores.max(1)
-            correct_count += (predictions == y).sum()
-            total += predictions.size(0)
-            
             if loader.dataset.train:
                 bar.set_description("Getting Accuracy for: Training Set")
             else:
+                loss = crit(scores, y)
+                testlosses.append(loss.item())
+                bar.set_postfix(loss= loss.item()) 
                 bar.set_description("Getting Accuracy for Testing Set")
-            
+                
+            _, predictions = scores.max(1)
+            correct_count += (predictions == y).sum()
+            total += predictions.size(0)
     model.train()
+    if loader.dataset.train == False:
+        avgloss = sum(testlosses)/len(testlosses)
+        avgtestloss.append(avgloss)
+        print(f"Avg Testing Loss: {avgloss:.2f}")
     return (correct_count/total) # Get back total accuracy on the data
 
 
 
-def checkpoint(state, epoch):
+def checkpointcreate(state, epoch):
     str1 = 'checkpoint_'
-    str2 = '.tar'
-    if (learning_rate == 1e-5):
-        str2 = '_Unfrozen.tar'
+    if (frozen == True):
+        str2 = '_frozen.tar'
     else:
-        str2 = '.tar'
+        str2 = '_.Unfrozen.tar'
     checkpoint_string = str1 + str(epoch) + str2
     checkpoint_filename = os.path.join(Direc, checkpoint_string)
     
     print("Checkpoint Saved.")
-    torch.save(state, checkpoint_filename)
+    torch.save({
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict()},
+        checkpoint_filename
+    )
 
     
 # All of these are appended after each epoch
@@ -174,145 +203,170 @@ Epoch_save = 0
 
 # Training the model
 def Train(num_epoch):
-    model.to(device)
-    for epoch in range(num_epoch):
-        model.train()
-        start_epoch = datetime.now()
-        losses = []
-        
-        bar = tqdm(enumerate(TrainingData), total=len(TrainingData), leave=False)
-        num_correct = 0
-        total_data = 0
-        
-        for batch_idx, (data, label) in bar:
-            data = data.to(device)
-            label = label.to(device)
+    total_train_hr = 0
+    total_train_min = 0
+    total_train_sec = 0
+    try:
+        model.to(device)
+        for epoch in range(num_epoch):
+            model.train()
+            start_epoch = datetime.now()
+            losses = []
             
-            optimizer.zero_grad()
+            bar = tqdm(enumerate(TrainingData), total=len(TrainingData), leave=False)
+            bar.set_description(f"Epoch {epoch + 1}/{num_epoch}")
+            num_correct = 0
+            total_data = 0
             
-            score = model(data)
-            _, prediction = score.max(1)
+            for batch_idx, (data, label) in bar:
+                data = data.to(device)
+                label = label.to(device)
+                
+                optimizer.zero_grad()
+                
+                score = model(data)
+                _, prediction = score.max(1)
+                
+                loss = crit(score, label)
+                loss.backward()
+                optimizer.step()
+                
+                losses.append(loss.item())
+                num_correct += (prediction==label).sum()
+                total_data += prediction.size(0)
+                
+                bar.set_postfix(loss= loss.item())
+                
+            # Get time difference in between each epoch
+            CurrentTime = datetime.now()
+            diff = CurrentTime - start_epoch
+            diff = diff.total_seconds()
             
-            loss = crit(score, label)
-            loss.backward()
-            optimizer.step()
+            hours = math.floor(diff / 60**2) # Get amount of hours
+            if hours > 0:
+                diff -= (hours * (60**2))
+            minutes = math.floor(diff / 60) # Get amount of minutes
+            if minutes > 0:
+                diff -= (minutes * 60)
+                
+            print(f"Time To Train: {hours} Hours, {minutes} Minutes, {diff:.2f} Seconds | Epoch {epoch+1}")
+            time = (f"{hours} | {minutes} | {diff:.2f}")
+            time_taken.append(time)
             
-            losses.append(loss.item())
-            num_correct += (prediction==label).sum()
-            total_data += prediction.size(0)
+            total_train_hr += (hours)
+            total_train_min += (minutes)
+            total_train_min += (diff)
+                
+            acctrain = get_accuracy(TrainingData, model)*100
+            acctest = get_accuracy(TestData, model)*100
+            avgloss = sum(losses)/len(losses)
+            print(f"Epoch {epoch + 1}/{num_epoch}: Accuracy [Used, Training, Test]: {num_correct/total_data*100:.2f}% / {acctrain:.2f}% / {acctest:.2f}% | Loss Avg: {avgloss:.3f}")
             
-            bar.set_description(f"Epoch {epoch + 1}/{num_epochs}")
-            bar.set_postfix(loss= loss.item())
+            # Get time difference in between each epoch
+            CurrentTime = datetime.now()
+            diff = CurrentTime - start_epoch
+            diff = diff.total_seconds()
             
-        acctrain = get_accuracy(TrainingData, model)*100
-        acctest = get_accuracy(TestData, model)*100
-        avgloss = sum(losses)/len(losses)
-        print(f"Epoch {epoch + 1}/{num_epochs}: Accuracy [Used, Training, Test]: {num_correct/total_data*100:.2f}% / {acctrain:.2f}% / {acctest:.2f}% | Loss Avg: {avgloss:.3f}")
-        
-        # Get time difference in between each epoch
-        CurrentTime = datetime.now()
-        diff = CurrentTime - start_epoch
-        diff = diff.total_seconds()
-        
-        hours = math.floor(diff / 60**2) # Get amount of hours
-        if hours > 0:
-            diff -= (hours * (60**2))
-        minutes = math.floor(diff / 60) # Get amount of minutes
-        if minutes > 0:
-            diff -= (minutes * 60)
+            hours = math.floor(diff / 60**2) # Get amount of hours
+            if hours > 0:
+                diff -= (hours * (60**2))
+            minutes = math.floor(diff / 60) # Get amount of minutes
+            if minutes > 0:
+                diff -= (minutes * 60)
+                
+            print(f"Time After Accuracy: {hours} Hours, {minutes} Minutes, {diff:.2f} Seconds | Epoch {epoch+1}")
             
-        print(f"Time: {hours} Hours, {minutes} Minutes, {diff:.2f} Seconds | Epoch {epoch+1}")
-        
-        # Processing Data to be Graphed and put into a Spreadsheet
-        time = (f"{hours} | {minutes} | {diff:.2f}")
-        time_taken.append(time)
-        
-        acctrain = acctrain.numpy(force=True)
-        acctrain = acctrain.astype(np.float16)
-        accuracy_trainset_over_time.append(acctrain)
-        
-        acctest = acctest.numpy(force=True)
-        acctest = acctest.astype(np.float16)
-        accuracy_test_over_time.append(acctest)
-        
-        loss_over_time.append(avgloss)
-        
-        # Saving the current model after each epoch (checkpoint)
-        checkpoint_save = {'state_dict' : model.state_dict(), 'optimizer' : optimizer.state_dict()}
-        checkpoint(checkpoint_save, epoch=(epoch+1))
-        print("")
-        
+            # Processing Data to be Graphed and put into a Spreadsheet
+            acctrain = acctrain.numpy(force=True)
+            acctrain = acctrain.astype(np.float16)
+            accuracy_trainset_over_time.append(acctrain)
+            
+            acctest = acctest.numpy(force=True)
+            acctest = acctest.astype(np.float16)
+            accuracy_test_over_time.append(acctest)
+            
+            loss_over_time.append(avgloss)
+            
+            # Saving the current model after each epoch (checkpoint)
+            checkpoint_save = {'state_dict' : model.state_dict(), 'optimizer' : optimizer.state_dict()}
+            checkpointcreate(checkpoint_save, epoch=(epoch+1))
+            print("")
+    except KeyboardInterrupt:
+        print("Early stop!")
+        pass
+    
 
 if Train_type == 1: # Before training, declare CrossEntropyLoss and Adam along with any learning rate at the time
     crit = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    Train(num_epoch=num_epochs)
+    if Trained_Model == 2:
+        model.classifier[6] = nn.Linear(4096, 100)
+    else:
+        model.fc = nn.Linear(2048, 100)
+        
+    if ContinueTrain:
+        filename = os.path.join(base_dir, 'continue.tar')
+        print("File found!")
+        checkpoint = torch.load(filename)
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        
+    Train(num_epochs)
     
 elif Train_type == 2:
-    if Trained_Model == 1: # For ResNet50
-        count = 0
-        for child in model.children():
-            count += 1
-            if count < 9: # Experimenting with 9, used 8 before so yeah
-                for param in child.parameters():
-                    param.requires_grad = False
-        model.fc = Classifier
-        
-    elif Trained_Model == 2: # For VGG16
-        count = 0
-        for param in model.parameters():
-            count += 1
-            if count < 28:
-                param.requires_grad = False
-        model.classifier[6] = Classifier
-        
-    elif Trained_Model == 3: # For InceptionV3
-        model.fc = Classifier
-        
-    print("Model Frozen, Training Fewer Layers")
-    crit = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    epochs = (num_epochs * (1/4))
-    Train(num_epoch=epochs)
-    
-elif Train_type == 3:
-        
     if Trained_Model == 1:
-        count = 0
-        for child in model.children():
-            count += 1
-            if count < 8:
-                for param in child.parameters():
-                    param.requires_grad = False
+        for param in model.parameters():
+            param.requires_grad = False
+                
         model.fc = Classifier
         
     if Trained_Model == 2:
+        for param in model.parameters():
+            param.requires_grad = False
         model.classifier[6] = Classifier
         
     elif Trained_Model == 3:
+        for param in model.parameters():
+            param.requires_grad = False
+        model.fc = Classifier
+        
+    print("Model Frozen, Training Fewer Layers | Training Method 2")
+    crit = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    
+    Train(num_epochs)
+    
+elif Train_type == 3:
+    if Trained_Model == 2:
+        for param in model.parameters():
+            param.requires_grad = False
+        model.classifier[6] = Classifier
+    else:
+        for param in model.parameters():
+            param.requires_grad = False
         model.fc = Classifier
         
     print("Model Frozen, Training Fewer Layers")
+    # Freeze everything except classifier and train with lower learning rate
     
     crit = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate/10)
 
-    epochs = (num_epochs * (3/4))
-    Train(num_epoch=epochs)
-    
-    # Unfreeze the whole model and retrain with a lower training rate
-    for child in model.children():
-        for param in child.parameters():
-            param.requires_grad = True
+    epochs = (num_epochs * (1/4))
+    frozen = True
+    Train(num_epoch=int(epochs))
+    frozen = False
+    # Unfreeze the whole model and retrain with a higher training rate
+    for param in model.parameters():
+        param.requires_grad = True
             
-    learning_rate = 1e-5
     print("Model Unfrozen, Adjusted Learning Rate.")
 
     crit = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     
     epochs = (num_epochs * (3/4))
-    Train(num_epoch=epochs)
+    Train(num_epoch=int(epochs))
     
 # After Training
 CurrentTime = datetime.now()
@@ -346,8 +400,8 @@ for x in range(num_epochs):
     Epoch.append(x+1)
     
 CSVDirec = os.path.join(Direc, 'data.csv')
-fields = ["Epoch", "Loss", "Training Accuracy", "Testing Accuracy", "Time Taken"]
-with open(CSVDirec, 'w') as new_file:
+fields = ["Epoch", "Loss", "Training Accuracy", "Testing Accuracy", "Testing Loss", "Time Taken"]
+with open(CSVDirec, 'w', newline='') as new_file:
     csv_writer = csv.DictWriter(new_file, fieldnames=fields, delimiter=",")
     csv_writer.writeheader()
     for x in range(len(loss_over_time)):
@@ -355,11 +409,22 @@ with open(CSVDirec, 'w') as new_file:
                              "Loss": loss_over_time[x],
                              "Training Accuracy": accuracy_trainset_over_time[x],
                              "Testing Accuracy": accuracy_test_over_time[x],
+                             "Testing Loss": avgtestloss[x],
                              "Time Taken": time_taken[x]})
     csv_writer.writerow({"Epoch": "Final Averages",
                          "Loss": "",
                          "Training Accuracy": Final_Train_Acc,
                          "Testing Accuracy": Final_Test_Acc,
                          "Time Taken": f"Total Time: {hours} Hours, {minutes} Minutes, {diff:.2f} Seconds"})
+    csv_writer.writerow({"Epoch": "",
+                         "Loss": "",
+                         "Training Accuracy": "",
+                         "Testing Accuracy": "",
+                         "Time Taken": f"Total Training Time: {total_train_hr} Hours, {total_train_min} Minutes, {total_train_sec:.2f} Seconds"})
 
 print("CSV Completed.")
+
+best_acc = max(accuracy_test_over_time)
+best_epoch = accuracy_test_over_time.index(best_acc)+1
+
+print(f"Best Accuracy on Test Set was {best_acc}% at epoch {best_epoch}")
